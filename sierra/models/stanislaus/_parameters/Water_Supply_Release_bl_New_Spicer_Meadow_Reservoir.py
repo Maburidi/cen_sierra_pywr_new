a3f763212e1845ff21eb2e8751d1bec058605a3d
Mohammed Aburidi <48891624+Maburidi@users.noreply.github.com>
2024-10-17 22:49:59 -0700

    Update Water_Supply_Release_bl_New_Spicer_Meadow_Reservoir.py

--- sierra/models/stanislaus/_parameters/Water_Supply_Release_bl_New_Spicer_Meadow_Reservoir.py
+++ sierra/models/stanislaus/_parameters/Water_Supply_Release_bl_New_Spicer_Meadow_Reservoir.py
@@ -8,14 +8,16 @@ class Water_Supply_Release_bl_New_Spicer_Meadow_Reservoir(BaseParameter):
 
     def _value(self, timestep, scenario_index):
         water_supply_reqt = self.model.tables["New Spicer Meadow District release"]  # cfs
+        
+
         # start = self.datetime.strftime('%d-%b')  # e.g.: 01-Jan
         month = self.datetime.month
         start = '{}/{}'.format(month, self.datetime.day)
         if self.model.mode == 'scheduling':
-            max_flow = water_supply_reqt[start] / 35.31
+            max_flow = water_supply_reqt.loc[start, 'P25' ] / 35.31
         else:
             end = '{}/{}'.format(month, self.days_in_month)
-            max_flow = water_supply_reqt[start:end].sum() / 35.31  # cfs to cms
+            max_flow = water_supply_reqt.loc[start:end,'P25' ].sum() / 35.31  # cfs to cms
 
         # modify max_flow up/down based on regression
         # y = -0.0299x2 + 0.5624x where x = SJVI and y = % of mean.
